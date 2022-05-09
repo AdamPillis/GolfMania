519347507ea066877bfafb0cfe372027ef2f8470
AdamPillis <adam.pillis@hotmail.com>
2022-05-09 12:44:01 +0000

    Add: list display functionality to admin panel and create all_products view

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,39 @@
 from django.contrib import admin
 from .models import Product, Category, Brand
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Brand)
+
+class ProductAdmin(admin.ModelAdmin):
+    """To customise products view via admin panel"""
+    list_display = (
+        'brand',
+        'type',
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class BrandAdmin(admin.ModelAdmin):
+    """To customise brand view via admin panel"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """To customise cateogry view via admin panel"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """
+    Renders products.html and displays
+    all products on website. Search and 
+    filter option will also be included.
+    """
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
