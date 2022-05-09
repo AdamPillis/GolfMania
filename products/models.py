eb44c5bb563199078420022c74e4192037d0f496
AdamPillis <adam.pillis@hotmail.com>
2022-05-09 12:05:04 +0000

    Add: features and hand vars to products model

--- products/models.py
+++ products/models.py
@@ -5,6 +5,11 @@ TYPE_STATUS = (
     ("Used", "Used")
     )
 
+HAND_TYPE = (
+    ("Left Hand", "Left Hand"),
+    ("Right Hand", "Right Hand")
+    )
+
 
 class Brand(models.Model):
     """
@@ -59,7 +64,11 @@ class Product(models.Model):
     type = models.CharField(
     choices=TYPE_STATUS, default=False, max_length=20
     )
+    hand = models.CharField(
+    choices=HAND_TYPE, default=False, max_length=20
+    )
     description = models.TextField()
+    features = models.TextField(null=True, blank=True)
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     has_quality = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
