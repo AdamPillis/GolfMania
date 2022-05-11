f8a9ec1fcee455df0ae4b08cd2ab57dc619a9be7
AdamPillis <adam.pillis@hotmail.com>
2022-05-11 10:48:27 +0000

    Add: category search functionality to main-nav.html

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -12,8 +12,18 @@ def all_products(request):
     """
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        # splitting into list after commas and filter list who's 
+        # category name is in the list
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            # double underscore for django's built in search
+            products = products.filter(category__name__in=categories)
+            # category objects created from search used to display them products.html
+            categories = Category.objects.filter(name__in=categories)
+
         if 'search' in request.GET:
             query = request.GET['search']
             if not query:
@@ -26,6 +36,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_word': query,
+        'chosen_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
