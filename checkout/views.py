e3f9233b6c611e95505f5a01341c3cbf65dd5353
AdamPillis <adam.pillis@hotmail.com>
2022-05-20 13:57:26 +0000

    Add: form submit functionality in stripe_elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,9 @@ import stripe
 
 def checkout(request):
     """X"""
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket at the moment")
@@ -20,13 +23,23 @@ def checkout(request):
     total = current_basket['order_total']
     # stripe requires amount to be charged as integer so *100
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     checkout_form = CheckoutForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+                Please make sure it is set in your envrionment correctly.')
+
     template = 'checkout/checkout.html'
     context = {
         'checkout_form': checkout_form,
-        'stripe_public_key': 'pk_test_51KngroIqDjTO0Fk0DppXT5thZtF0JaI0gfxwhTE2wd13JzvfIi4pFNYrJf3JB8bMwtaszxEN6uJoeYgwehbdrUJi00BkNkfTkn',
-        'client_secret': 'test_client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
