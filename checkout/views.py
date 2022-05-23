3d2360a1b61e091c88ceb07311539c8490bdf473
AdamPillis <adam.pillis@hotmail.com>
2022-05-23 09:32:57 +0000

    Update: checkout view to save post data from checkout_form

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,9 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import CheckoutForm
+from .models import Order, OrderLineItem
+from products.models import Product
+
 from basket.contexts import basket_contents
 
 import stripe
@@ -13,10 +16,65 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    basket = request.session.get('basket', {})
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
+
+        form_data = {
+            'title': request.POST['title'],
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'house_number': request.POST['house_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_city': request.POST['town_city'],
+            'county': request.POST['county'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'delivery_instructions': request.POST['delivery_instructions'],
+        }
+        checkout_form = CheckoutForm(form_data)
+        if checkout_form.is_valid():
+            checkout_form.save()
+            for item_id, item_data in basket.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_basket'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There is an error with your form. \
+                Please try again.')
+    else:
+        basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('products'))
+
 
     # to get the current total of session bag for stripe payment
     current_basket = basket_contents(request)
