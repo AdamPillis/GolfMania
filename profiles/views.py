ea50b7b47776cb05fe526489d203fc4c6ee9f5dc
AdamPillis <adam.pillis@hotmail.com>
2022-05-25 10:18:28 +0000

    Add: urls.py and link profiles app with project and create profile.html

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Profile
+# from .forms import UserProfileForm
+
+from checkout.models import Order
+
+
+@login_required
+def profile(request):
+    """
+    Display the user's profile
+    """
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
+
