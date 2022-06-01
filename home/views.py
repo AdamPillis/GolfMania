7b0e401a6b2ae82457bf232953a6979d85c6f74d
AdamPillis <adam.pillis@hotmail.com>
2022-06-01 10:15:50 +0000

    Add: new_in showcase within product_detail.html

--- home/views.py
+++ home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from reviews.models import Review
+from products.models import Product
 
 
 def index(request):
     """View to render index.html template"""
+    products = Product.objects.all()
 
     context = {
+        'products': products,
         'reviews': Review.objects.filter(status=1).order_by('-created_on')
     }
     return render(request, 'home/index.html', context)
