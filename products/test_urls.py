16b3d91a379e1d8d6a0fa6f824c39ec2f534356c
AdamPillis <adam.pillis@hotmail.com>
2022-06-06 21:01:56 +0000

    Start: testing products/urls.py

---
+++ products/test_urls.py
@@ -0,0 +1,35 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import all_products, product_detail, add_product, update_product, delete_product
+
+
+class TestProductUrls(SimpleTestCase):
+    """
+    Testing all products urls set up within urls.py and
+    making sure that the url path name matches
+    the view function called
+    """
+    def test_product_url(self):
+        """Testing products/ products url (function = url name)"""
+        url = reverse('products')
+        self.assertEquals(resolve(url).func, all_products)
+
+    # def test_product_detail_url(self):
+    #     """Testing products/ product_detail url (function = url name)"""
+    #     url = reverse('products', kwargs=['product_id'])
+    #     self.assertEquals(resolve(url).func, product_detail)
+
+    def test_add_product_url(self):
+        """Testing products/ add_product url (function = url name)"""
+        url = reverse('add_product')
+        self.assertEquals(resolve(url).func, add_product)
+
+    # def test_update_product_url(self):
+    #     """Testing products/ update_product url (function = url name)"""
+    #     url = reverse('update_product', args=['product_id'])
+    #     self.assertEquals(resolve(url).func, update_product)
+    
+    # def test_delete_product_url(self):
+    #     """Testing products/ delete_product url (function = url name)"""
+    #     url = reverse('delete_product', args=['product_id'])
+    #     self.assertEquals(resolve(url).func, delete_product)
